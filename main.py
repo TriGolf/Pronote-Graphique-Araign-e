f50ee90954f54059572f2986493c7311196d270f
TriGolf <157053568+TriGolf@users.noreply.github.com>
2024-01-25 18:46:00 +0100

    Update main.py
    Add averages of the class

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import getpass
 while True :
     try :
         utilisateur = input('Veuillez entrer votre nom d\'utilisateur : ')
-        mot_de_passe = getpass.getpass("Veuillez entrer votre mot de passe : ")
+        mot_de_passe = getpass.getpass("Veuillez entrer votre mot de passe (c'est normal qu'il ne s'affiche pas) : ")
 
         client = pronotepy.Client(
             'https://0450047g.index-education.net/pronote/eleve.html',
@@ -17,7 +17,7 @@ while True :
             ent=ac_orleans_tours)
         
         print(f'Vous êtes conncté(e) en tant que {client.info.name} !')
-        
+
         break
     except :
         print("Les identifiants sont invalides, veuillez rééssayer")
@@ -31,6 +31,7 @@ class_averages = []
 for moyenne in client.current_period.averages :
     averages.append(moyenne.student)
     subjects.append(moyenne.subject.name)
+    class_averages.append(moyenne.class_average)
 
 
 def convert(liste) :
@@ -41,12 +42,22 @@ def convert(liste) :
     return liste
 
 averages = convert(averages)
+class_averages = convert(class_averages)
+
+groupe = []
+for x in range(len(subjects)*2) :
+    if x <= len(subjects)-1 :
+        groupe.append('Vos moyennes')
+    
+    else : groupe.append('Moyennes de classe')
+
 df = pd.DataFrame(dict(
-    value = averages,
-    variable = subjects))
+    value = averages+class_averages,
+    variable = subjects+subjects,
+    group = groupe))
            
 fig = px.line_polar(df, r = 'value', theta = 'variable', line_close = True,
-                    markers = True,text = 'value')
+                    markers = True,text = 'value', color = 'group')
 
 
 title = f"Moyenne générale : {client.current_period.overall_average} ||  Moyenne générale de la classe : {client.current_period.class_overall_average}"
@@ -63,7 +74,7 @@ fig.update_layout(
     title_y=0.02,  # Position verticale du titre (au-dessus du graphique)
 )
 
-fig.update_traces(textposition = 'top center')
+fig.update_traces(textposition = 'top center', fill = 'toself')
 
 fig.show() 
 
